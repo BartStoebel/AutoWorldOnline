75e4c9eaa031d7cab19c5c72fb0b4e8ba6f72677
Bart.Stoebel <Bart.Stoebel@gmail.com>
2017-03-01 09:29:12 +0100

    Eerste commit na import Apache en Hamcrest in C:\JavaLib Klasse Nummerplaat bijgewerkt volgens opgave (samen met Frank) Java bin op 1.8 gezet --> Testen klasse verloopt zonder foutmelding

--- src/be/vdab/voertuigen/div/Nummerplaat.java
+++ src/be/vdab/voertuigen/div/Nummerplaat.java
@@ -5,15 +5,59 @@
 
 package be.vdab.voertuigen.div;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author frank.roelants
  */
-public class Nummerplaat {
-    private String plaat;
+public class Nummerplaat implements Serializable, Comparable<Nummerplaat>{
+    private static long serialVersionUID = 1L;
+    private final String plaat;
 
-    public Nummerplaat(String plaat) {
+    Nummerplaat(String plaat) {
         this.plaat = plaat;
     }
+
+    public String getPlaat() {
+        return plaat;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.plaat);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Nummerplaat other = (Nummerplaat) obj;
+        if (!Objects.equals(this.plaat, other.plaat)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return plaat;
+    }
+
+    @Override
+    public int compareTo(Nummerplaat o) {
+        return plaat.compareTo(o.plaat);
+    }
+
     
 }
